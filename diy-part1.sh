53d2f3d4126978fd6684753acb86bd06b0f1dee8
hezhiming <hezhimingabc@qq.com>
2022-08-16 23:40:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # ./scripts/feeds update kenzo
 # ./scripts/feeds update small
 # ./scripts/feeds install -a -f -p kenzo
@@ -27,7 +27,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i "/small/d" "feeds.conf.default"
 
 sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 # ./scripts/feeds update helloworld
 # ./scripts/feeds install -a -f -p helloworld
 # sed -i "/helloworld/d" "feeds.conf.default"
