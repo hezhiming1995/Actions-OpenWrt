d87484bce2880f18beb7d1f66ccac5bcca39471c
hezhiming <hezhimingabc@qq.com>
2022-08-28 12:56:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # ./scripts/feeds update kenzo
